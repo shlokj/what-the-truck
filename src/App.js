857cffa4c29597a0ea7da51a5e20d98cc2f55c48
Ryan Kim <ryanswkim2003@gmail.com>
2022-10-27 19:49:09 -0700

    cleaned src and imported bootstrap & sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App vh-100'>
+      <div className='bg-success h-50 d-flex justify-content-center align-items-center'>
+        Testing
+      </div>
     </div>
   );
 }
