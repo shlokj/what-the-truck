d656800d4aec40dcc6aceee477d0b9fb16fff0d5
Shlok Jhawar <shlokj@gmail.com>
2022-11-10 19:46:50 -0800

    initial routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-// import { Login } from "./components/login.jsx";
-// import { SignUp } from "./components/signup.jsx";
-import { LoginSignupTab } from "./components/login_signup_tab.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./custom.scss";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./components/screens/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <LoginSignupTab />
+    <div>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
