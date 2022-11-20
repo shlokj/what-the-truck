c5a474c238f15b80b57f359a862f18b45d3e515b
Shlok Jhawar <shlokj@gmail.com>
2022-11-19 18:26:48 -0800

    firebase init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBozYjNDDJMDDf_Za2sUB8gqHpOOIutUEU",
+  authDomain: "what-the-truck-ucla.firebaseapp.com",
+  projectId: "what-the-truck-ucla",
+  storageBucket: "what-the-truck-ucla.appspot.com",
+  messagingSenderId: "855501075279",
+  appId: "1:855501075279:web:b4fbb86a771d81345e0b3a",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
