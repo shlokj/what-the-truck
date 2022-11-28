e986ec8f4e0382993d1260a06d09e1dd42821a20
Anmol Gupta <anmolgupta23@ucla.edu>
2022-11-27 19:50:51 -0800

    center align textbox & create file upload button

--- frontend/src/components/food_truck_review.jsx
+++ frontend/src/components/food_truck_review.jsx
@@ -2,6 +2,17 @@ import { Stack, Button, Rating, TextField } from "@mui/material";
 import React, { useState } from "react";
 // TODO: change write your review customized to food truck page -> Line 17
 
+const styles = {
+  heading: { margin: 32 },
+  textbox: { marginTop: 32, marginBottom: 14, textAlign: "left" },
+  fileUpload: {
+    textAlign: "center",
+    marginTop: 32,
+    marginBottom: 32,
+    marginLeft: 120,
+  },
+};
+
 export default function ReviewInput() {
   const [reviewText, setReviewText] = useState("");
 
@@ -10,14 +21,12 @@ export default function ReviewInput() {
       <Stack direction="column" spacing={1}>
         <div className="reviewCard">
           <div className="title">
-            <h1>write your review</h1>
+            <h1 align="center">Write a review here!</h1>
           </div>
-          <div className="descriptors">
-            <h3> write your review of the food truck below </h3>
-          </div>
+
           <div className="textField">
             <TextField
-              style={{ textAlign: "left" }}
+              style={styles.textbox}
               fullWidth
               label="Review"
               id="fullWidth"
@@ -25,32 +34,29 @@ export default function ReviewInput() {
               hintText="Message Field"
               floatingLabelText="MultiLine and FloatingLabel"
               multiline
-              rows={5}
+              rows={8}
               onChange={(e) => setReviewText(e.target.value)}
             />
           </div>
           <div className="descriptors">
-            <h3> attach any images you have in the box below </h3>
+            <h3 style={styles.heading}>Upload an image</h3>
           </div>
-          <div className="textField">
-            <TextField
-              style={{ textAlign: "left" }}
-              fullWidth
-              label="Images"
-              id="fullWidth"
-              default="attach images"
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiline
-              rows={5}
-            />
-          </div>
+
+          <form
+            id="upload_form"
+            enctype="multipart/form-data"
+            method="post"
+            style={styles.fileUpload}
+          >
+            <input type="file" name="file1" id="file1" />
+          </form>
+
           <div className="descriptors">
-            <h3>rate the food truck out of 5 stars</h3>
+            <h3 style={styles.heading}>Rate the food truck out of 5 stars</h3>
             <Rating size="large"></Rating>
           </div>
-          <div className="button">
-            <Button variant="contained">submit Your Review</Button>
+          <div className="button" align="center">
+            <Button variant="contained">Submit Review</Button>
           </div>
         </div>
       </Stack>
