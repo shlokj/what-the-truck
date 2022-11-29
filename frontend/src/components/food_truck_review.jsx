66fb35ec05c040e8e96b338fa25e3754506d215f
Sidharth Sudhir <sidharthsudhir12@gmail.com>
2022-11-29 12:46:41 -0800

    modified styling of review input card and css file

--- frontend/src/components/food_truck_review.jsx
+++ frontend/src/components/food_truck_review.jsx
@@ -1,17 +1,52 @@
-import { Stack, Button, Rating, TextField, Paper, Grid } from "@mui/material";
+import { Stack, Button, Rating, TextField, Paper, Grid, autocompleteClasses } from "@mui/material";
 import React, { useState } from "react";
 // TODO: change write your review customized to food truck page -> Line 17
 
 const styles = {
-  heading: { margin: 32 },
-  textbox: { marginTop: 32, marginBottom: 14, textAlign: "left" },
+  heading: { 
+    margin: 32,
+  },
+
   fileUpload: {
     textAlign: "center",
     marginTop: 32,
     marginBottom: 32,
     marginLeft: 120,
   },
-  paper: { paddingTop: 32, paddingLeft: "25%", align: "center" },
+
+  paper: 
+  { paddingTop: 32, 
+    paddingLeft: "25%", 
+    align: "center" 
+  },
+
+  button: {
+    paddingTop: '40px',
+    paddingBottom: '30px'
+  },
+
+  textField: {
+    marginTop: 32, 
+    marginBottom: 14, 
+    marginLeft: 150,
+    marginRight: 150,
+    width: 400,
+    textAlign: 'left',
+    fontFamily:'Cambria, Cochin, Georgia, Times, Times New Roman, serif',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: '30px',
+    margin: '0px',
+  },
+
+  descriptors: {
+    fontWeight: 'lighter',
+    textAlign: 'center',
+    margin: 'auto'
+  }
+
 };
 
 export default function ReviewInput() {
@@ -29,7 +64,7 @@ export default function ReviewInput() {
 
               <div className="textField">
                 <TextField
-                  style={styles.textbox}
+                  style={styles.textField}
                   fullWidth
                   label="Review"
                   id="fullWidth"
@@ -42,7 +77,7 @@ export default function ReviewInput() {
                 />
               </div>
               <div className="descriptors">
-                <h3 style={styles.heading}>Upload an image</h3>
+                <h3 style={styles.heading} align="center">Upload an image</h3>
               </div>
 
               <form
@@ -55,12 +90,14 @@ export default function ReviewInput() {
               </form>
 
               <div className="descriptors">
-                <h3 style={styles.heading}>
+                <h3 style={styles.heading} align="center">
                   Rate the food truck out of 5 stars
                 </h3>
-                <Rating size="large"></Rating>
+                <div align="center">
+                  <Rating size="large"></Rating>
+                </div>
               </div>
-              <div className="button" align="center">
+              <div className="button" align="center" style={styles.button}>
                 <Button variant="contained">Submit Review</Button>
               </div>
             </div>
