53d6d5796b8b8811ec836f9e1b44f22d62db5ef4
Anmol Gupta <anmolgupta23@ucla.edu>
2022-11-29 01:02:26 -0800

    added paper behind review page

--- frontend/src/components/food_truck_review.jsx
+++ frontend/src/components/food_truck_review.jsx
@@ -1,4 +1,4 @@
-import { Stack, Button, Rating, TextField } from "@mui/material";
+import { Stack, Button, Rating, TextField, Paper, Grid } from "@mui/material";
 import React, { useState } from "react";
 // TODO: change write your review customized to food truck page -> Line 17
 
@@ -11,55 +11,62 @@ const styles = {
     marginBottom: 32,
     marginLeft: 120,
   },
+  paper: { paddingTop: 32, paddingLeft: "25%", align: "center" },
 };
 
 export default function ReviewInput() {
   const [reviewText, setReviewText] = useState("");
 
   return (
-    <Stack spacing={5}>
-      <Stack direction="column" spacing={1}>
-        <div className="reviewCard">
-          <div className="title">
-            <h1 align="center">Write a review here!</h1>
-          </div>
+    <Grid container style={styles.paper}>
+      <Stack spacing={5}>
+        <Paper elevation={20}>
+          <Stack direction="column" spacing={1}>
+            <div className="reviewCard">
+              <div className="title">
+                <h1 align="center">Write a review here!</h1>
+              </div>
 
-          <div className="textField">
-            <TextField
-              style={styles.textbox}
-              fullWidth
-              label="Review"
-              id="fullWidth"
-              default="your review here..."
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiline
-              rows={8}
-              onChange={(e) => setReviewText(e.target.value)}
-            />
-          </div>
-          <div className="descriptors">
-            <h3 style={styles.heading}>Upload an image</h3>
-          </div>
+              <div className="textField">
+                <TextField
+                  style={styles.textbox}
+                  fullWidth
+                  label="Review"
+                  id="fullWidth"
+                  default="your review here..."
+                  hintText="Message Field"
+                  floatingLabelText="MultiLine and FloatingLabel"
+                  multiline
+                  rows={8}
+                  onChange={(e) => setReviewText(e.target.value)}
+                />
+              </div>
+              <div className="descriptors">
+                <h3 style={styles.heading}>Upload an image</h3>
+              </div>
 
-          <form
-            id="upload_form"
-            enctype="multipart/form-data"
-            method="post"
-            style={styles.fileUpload}
-          >
-            <input type="file" name="file1" id="file1" />
-          </form>
+              <form
+                id="upload_form"
+                enctype="multipart/form-data"
+                method="post"
+                style={styles.fileUpload}
+              >
+                <input type="file" name="file1" id="file1" />
+              </form>
 
-          <div className="descriptors">
-            <h3 style={styles.heading}>Rate the food truck out of 5 stars</h3>
-            <Rating size="large"></Rating>
-          </div>
-          <div className="button" align="center">
-            <Button variant="contained">Submit Review</Button>
-          </div>
-        </div>
+              <div className="descriptors">
+                <h3 style={styles.heading}>
+                  Rate the food truck out of 5 stars
+                </h3>
+                <Rating size="large"></Rating>
+              </div>
+              <div className="button" align="center">
+                <Button variant="contained">Submit Review</Button>
+              </div>
+            </div>
+          </Stack>
+        </Paper>
       </Stack>
-    </Stack>
+    </Grid>
   );
 }
