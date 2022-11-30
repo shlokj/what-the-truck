1d229298ed570fe655064248b5188572acf88ae9
Shubh Kathuria <shubh.k1402@gmail.com>
2022-11-29 17:54:32 -0800

    yoyoyo

--- frontend/src/components/food_truck_review.jsx
+++ frontend/src/components/food_truck_review.jsx
@@ -1,8 +1,69 @@
-import { Stack, Button, Rating, TextField } from "@mui/material";
+import { Stack, Button, Rating, TextField, Paper, Grid, autocompleteClasses } from "@mui/material";
 import React, { useState } from "react";
 import { collection, addDoc, doc } from "firebase/firestore";
 import { db } from "..";
 import { useParams } from "react-router-dom";
+
+
+const styles = {
+  heading: { 
+    margin: 32,
+    marginTop: 50,
+    marginBottom: 20
+  },
+
+  fileUpload: {
+    textAlign: "center",
+    marginTop: 32,
+    marginBottom: 32,
+    marginLeft: 120,
+  },
+
+  paper: { 
+    paddingTop: 32, 
+    paddingLeft: "25%", 
+    align: "center",
+    borderRadius: 40
+  },
+
+  button: {
+    paddingTop: '40px',
+    paddingBottom: '30px'
+  },
+
+  textField: {
+    marginTop: 32, 
+    marginLeft: 150,
+    marginRight: 150,
+    align: "center",
+    width: 400,
+    textAlign: 'left',
+    fontFamily:'Cambria, Cochin, Georgia, Times, Times New Roman, serif',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: '30px',
+    margin: '0px',
+  },
+
+  reviewCard: {
+    alignItems: 'center',
+    borderRadius: 50,
+    width: 700,
+    margin: 'auto',
+    marginTop: 50,
+    marginBottom: 50
+  },
+
+  descriptors: {
+    fontWeight: 'lighter',
+    textAlign: 'center',
+    margin: 'auto',
+    marginTop: 20,
+  }
+
+};
 
 
 export default function ReviewInput() {
@@ -54,59 +115,62 @@ export default function ReviewInput() {
 
 
   return (
-    <Stack spacing={5}>
-      <Stack direction="column" spacing={1}>
-        <div className="reviewCard">
-          <div className="title">
-            <h1>write your review</h1>
-          </div>
-          <div className="descriptors">
-            <h3> write your review of the food truck below </h3>
-          </div>
-          <div className="textField">
-            <TextField
-              style={{ textAlign: "left" }}
-              fullWidth
-              label="Review"
-              id="fullWidth"
-              default="your review here..."
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiline
-              rows={5}
-              onChange={(e) => setReviewText(e.target.value)}
-            />
-          </div>
-          <div className="descriptors">
-            <h3> attach any images you have in the box below </h3>
-          </div>
-          <div className="textField">
-            <TextField
-              style={{ textAlign: "left" }}
-              fullWidth
-              label="Images"
-              id="fullWidth"
-              default="attach images"
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiline
-              rows={5}
-            />
-          </div>
-          <div className="descriptors">
-            <h3>rate the food truck out of 5 stars</h3>
-            <Rating size="large"></Rating>
-          </div>
-          <div className="button">
-            <Button 
-              variant="contained"
-              onClick={handleSubmit}
-            >
-                submit Your Review
-              </Button>
-          </div>
-        </div>
+    <Grid container style={styles.paper}>
+      <Stack spacing={5}>
+        <Paper elevation={20}>
+          <Stack direction="column" spacing={1}>
+            <div className="reviewCard" style={styles.reviewCard}>
+              <div className="title">
+                <h1 align="center">Write a review here!</h1>
+              </div>
+
+              <div className="textField">
+                <TextField
+                  style={styles.textField}
+                  fullWidth
+                  label="Review"
+                  id="fullWidth"
+                  default="your review here..."
+                  hintText="Message Field"
+                  align="center"
+                  floatingLabelText="MultiLine and FloatingLabel"
+                  multiline
+                  rows={8}
+                  onChange={(e) => setReviewText(e.target.value)}
+                />
+              </div>
+              <div className="descriptors">
+                <h3 style={styles.heading} align="center">Upload an image</h3>
+              </div>
+
+              <form
+                id="upload_form"
+                enctype="multipart/form-data"
+                method="post"
+                style={styles.fileUpload}
+              >
+                <input type="file" name="file1" id="file1" />
+              </form>
+
+              <div className="descriptors">
+                <h3 style={styles.heading} align="center">
+                  Rate the food truck out of 5 stars
+                </h3>
+                <div align="center">
+                  <Rating size="large"></Rating>
+                </div>
+              </div>
+              <div className="button" align="center" style={styles.button}>
+                <Button 
+                  variant="contained"
+                  onClick={handleSubmit}
+                  >
+                    Submit Review</Button>
+              </div>
+            </div>
+          </Stack>
+        </Paper>
       </Stack>
-    </Stack>
+    </Grid>
   );
 }
