d06077dd4f50775c19d0a63110a13693d42bc0e7
Shubh Kathuria <shubh.k1402@gmail.com>
2022-11-30 03:05:19 -0800

    fetched truck name and description

--- frontend/src/screens/FoodtruckPage.jsx
+++ frontend/src/screens/FoodtruckPage.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 import { ImageCarousel } from "../components";
 import { ReviewsList } from "../components";
 import { useParams } from "react-router-dom";
-import { collection, doc, getDocs } from 'firebase/firestore';
+import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
 import { db } from "..";
 // import StarIcon from "@mui/icons-material/Star";
 
@@ -48,6 +48,47 @@ export default function FoodtruckPage() {
  });
 
 
+ async function getDesc(){
+    const docRef = doc(db, "Trucks", foodTruckName);
+    try {
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()) {
+          return (docSnap.data().Description);
+      } else {
+          console.log("Document does not exist")
+      }
+
+    } catch(error) {
+        console.log(error)
+    }
+  }
+
+  var desc = getDesc().then(function(result) {
+    let truckDescription = result;
+    console.log(truckDescription);
+  });
+
+  async function getName(){
+    const docRef = doc(db, "Trucks", foodTruckName);
+    try {
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()) {
+          return (docSnap.data().Name);
+      } else {
+          console.log("Document does not exist")
+      }
+
+    } catch(error) {
+        console.log(error)
+    }
+  }
+
+  var desc = getName().then(function(result) {
+    let truckName = result;
+    console.log(truckName);
+  });
+
+  
   const [value, setValue] = useState(3.5); // replace 4 with variable that displays average rating of food truck
   return (
     <Paper style={{ maxHeight: "100vh", overflow: "auto" }}>
