07db39da7b7d06deb91e1cbb63ee164c9c0cd472
Ryan Kim <ryanswkim2003@gmail.com>
2022-11-30 00:17:32 -0800

    completely revamped carousel component, changed styling to fix some layout issues, and integrated new carousel component in food truck page

--- frontend/src/screens/FoodtruckPage.jsx
+++ frontend/src/screens/FoodtruckPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Grid, Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import "bootstrap/dist/css/bootstrap.css";
-// import { Image } from "mui-image";
 import { ImageCarousel } from "../components";
 import { ReviewsList } from "../components";
 import { useParams } from "react-router-dom";
@@ -23,46 +21,40 @@ export default function FoodtruckPage() {
   const foodTruckName = useParams()
     .foodTruckName.replace(/[^A-Za-z0-9]/g, "")
     .toLowerCase();
+
   const [value, setValue] = React.useState(2);
   return (
-    <Paper style={{ maxHeight: "100vh", overflow: "auto" }}>
-      <Grid container justifyContent="center">
-        <div style={styles.image}>
-          <img
-            height="450px"
-            style={{ borderRadius: "16px" }}
-            src="../../creamyboyslogo.jpeg"
-            alt="creamy boys logo"
-          />
-        </div>
-        <div className="ImageCarousel">
-          <ImageCarousel />
-        </div>
-        <div>
-          <Stack
-            direction="row"
-            spacing={2}
-            marginTop={4}
-            // justifyContent="center"
-          >
-            <Button variant="outlined">Review this Foodtruck</Button>
-            <Box>
-              <Typography component="legend">Average Rating</Typography>
-              <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              />
-            </Box>
-            <h2>Today: Rieber 11 am- 2:30 pm, Sproul 9 pm - 12 am</h2>
-          </Stack>
-        </div>
-        <div classname="ReviewsList">
-          <ReviewsList />
-        </div>
-      </Grid>
-    </Paper>
+    <div className="d-flex flex-column justify-content-between gap-3 align-items-center">
+      <div style={styles.image}>
+        <img
+          height="450px"
+          style={{ borderRadius: "16px" }}
+          src="../../creamyboyslogo.jpeg"
+          alt="creamy boys logo"
+        />
+      </div>
+      <div style={{ width: "30%", height: "30%" }}>
+        <ImageCarousel />
+      </div>
+      <div>
+        <Stack direction="row" spacing={2} marginTop={4}>
+          <Button variant="outlined">Review this Foodtruck</Button>
+          <Box>
+            <Typography component="legend">Average Rating</Typography>
+            <Rating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+          </Box>
+          <h2>Today: Rieber 11 am- 2:30 pm, Sproul 9 pm - 12 am</h2>
+        </Stack>
+      </div>
+      <div classname="ReviewsList">
+        <ReviewsList />
+      </div>
+    </div>
   );
 }
