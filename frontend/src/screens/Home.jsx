79489397a2dcf102949c90b34496faae19755db0
Sidharth Sudhir <sidharthsudhir12@gmail.com>
2022-11-30 21:33:59 -0800

    made final changes to homepage header and footer

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -25,6 +25,7 @@ const fontstyle = {
 
 const image = {
   paddingBottom: 20,
+  width: 400
 };
 
 const placeholderText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -75,10 +76,13 @@ export default function Home() {
         <div className="w-100 d-flex justify-content-around">
           <div className="d-flex justify-content-between align-items-center gap-2">
             <div>
-              <img src="/public/truckLogo.png"></img>
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" 
+            fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+              <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+              </svg>
             </div>
-            <div style={fontstyle}>
-              <h1>What the Truck</h1>
+            <div style={fontstyle} className="d-flex flex-row">
+              <h1> What the Truck</h1>
             </div>
           </div>
           <Button
@@ -222,7 +226,7 @@ export default function Home() {
       >
         <div className="d-flex flex-column align-items-center" style={column}>
           <h4>What the Truck</h4>
-          <div className="align-items-left" style={image}>
+          <div className="align-items-left" >
             <img
               src="logo.jpeg"
               alt="logo"
@@ -231,11 +235,28 @@ export default function Home() {
           </div>
         </div>
         <div style={column}>
-          <h4>
+          <h4 align="center">
             <u>Our Mission:</u>
           </h4>
-          <div className="align-items-left" style={image}>
-            Hello
+          <div className="justify-content-end" style={image}>
+          <p>
+              What The Truck is a service - built by and for students at UCLA - that 
+            aims to ease the navigation of food truck options, menus, and schedules.
+          </p>
+
+           <p>
+              Please keep in mind that any changes occuring on UCLA Dining's page may 
+              take a few days to reflect on our website. If you would like to report a bug,
+              please any of the given email IDs.
+          </p>
+            <p>
+              <h7> 
+                built with shlok's {}
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="white" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                </svg>   in the olympic study lounge
+              </h7>
+            </p>
           </div>
         </div>
         <div style={column}>
