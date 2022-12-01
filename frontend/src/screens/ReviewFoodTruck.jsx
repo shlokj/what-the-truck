4a45c477754866c61d12baa6ce7b64b286f33cba
Sidharth Sudhir <sidharthsudhir12@gmail.com>
2022-11-30 22:16:15 -0800

    divided code from homepage into component and added to food truck and review page

--- frontend/src/screens/ReviewFoodTruck.jsx
+++ frontend/src/screens/ReviewFoodTruck.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { ReviewInput } from "../components";
+import Footer from "../components/footer";
 
 export default function ReviewFoodTruck() {
   return (
     <div className="ReviewFoodTruck">
       <ReviewInput />
+      <Footer>
+
+      </Footer>
     </div>
   );
 }
