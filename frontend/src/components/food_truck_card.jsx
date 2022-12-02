d3a9104964bb0930aabc395328016c4b7394a07a
Ryan Kim <ryanswkim2003@gmail.com>
2022-12-01 19:06:09 -0800

    sorting for home page

--- frontend/src/components/food_truck_card.jsx
+++ frontend/src/components/food_truck_card.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Button } from "@mui/material";
+import { Button, Rating } from "@mui/material";
 
-export default function FoodTruckCard({ name, text, imageURL }) {
+export default function FoodTruckCard({
+  name,
+  text,
+  imageURL,
+  rating,
+  numberOfReviews,
+}) {
   return (
-    <div className="bg-warning p-3 py-4 d-flex justify-content-center border border-2 border-primary rounded-3 gap-4">
+    <div className="position-relative bg-warning p-3 py-4 d-flex justify-content-center border border-2 border-primary rounded-3 gap-4">
       <div
         className="w-50 d-flex align-self-center justify-content-center align-items-center overflow-hidden rounded"
         style={{ height: "35vh" }}
@@ -17,14 +23,22 @@ export default function FoodTruckCard({ name, text, imageURL }) {
         />
       </div>
 
-      <div className="w-50 h-100 d-flex flex-column align-items-center gap-2">
+      <div className="position-relative w-50 h-100 d-flex flex-column align-items-center gap-2">
         <Button
           className="w-75 text-light bg-primary rounded-4 px-3 py-2 font-weight-bold"
           sx={{ fontWeight: "bold" }}
         >
           {name}
         </Button>
-        <div className="p-1">{text}</div>
+        <div className="p-1 pb-3 mb-4">{text}</div>
+      </div>
+      <div className="d-flex w-50 justify-content-between position-absolute bottom-0 end-0">
+        <div className="d-flex mb-3 ms-2 justify-content-center align-items-center bg-primary rounded rounded-3 px-3 py-1 text-light">
+          Number of Reviews: {numberOfReviews}
+        </div>
+        <div className="d-flex mb-3 me-3 justify-content-center align-items-center bg-primary rounded rounded-3 px-3 py-1">
+          <Rating value={rating} precision={0.5} readOnly />
+        </div>
       </div>
     </div>
   );
